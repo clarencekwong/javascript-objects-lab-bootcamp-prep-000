1719d21dd66a1eda2f6c0a31a276536faaf8e290
Clarence Kwong <clarencekwong7@gmail.com>
2018-03-15 16:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj,key,val) {
 }
 
 function deleteFromObjectByKey(obj,key) {
-  
+  var newObj = Object.assign({},obj)
+  delete newObj[key]
+  return newObj
 }
