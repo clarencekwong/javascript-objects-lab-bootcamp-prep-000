6fb500f7560225a29712e296baee751bb62b4c90
Clarence Kwong <clarencekwong7@gmail.com>
2018-03-15 16:39:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {}
 function updateObjectWithKeyAndValue (obj,key,val) {
   return Object.assign({[key]:val},obj)
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,val) {
+  obj.key = val
+  return obj
+}
